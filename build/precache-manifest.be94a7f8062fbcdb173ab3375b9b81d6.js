8fe519cdf77905c1a880e9db8fa2e8f0ffc15889
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2020-07-08 00:04:46 +0700

    Added build folder

---
+++ build/precache-manifest.be94a7f8062fbcdb173ab3375b9b81d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12df085d0895c3b962d1fa9dc90c9f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab66c37b833c1916bf8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5a8d0e5f3327c41e2ecf",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fab66c37b833c1916bf8",
+    "url": "/static/js/2.a56c51ce.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/static/js/2.a56c51ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8d0e5f3327c41e2ecf",
+    "url": "/static/js/main.2a0249f1.chunk.js"
+  },
+  {
+    "revision": "059d0658814faf888e00",
+    "url": "/static/js/runtime-main.eec48e40.js"
+  }
+]);
